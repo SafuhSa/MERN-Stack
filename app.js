68e89a1f70d6bb556784f43076df3eb1d4060c0e
Safuh <Safoh95@windowslive.com>
2018-12-25 15:57:59 -0800

    mern stack setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+
+
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+  
+app.get("/", (req, res) => res.send("Hello Safuh"));
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
